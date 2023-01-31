5558aabcd0efc26a1098c1a20bc0a12d78b7c7ba
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-01-31 19:25:27 +0530

    Add files via upload

---
+++ Frontend-main/frontend/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="text-center p-6 bg-gray-200">
+        <span>© 2023 </span>
+        <a className="text-gray-600 font-semibold" href="/">
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
